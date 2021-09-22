4a8d0b6ccc1927348459a404c2106aa74aae9822
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-09-22 01:10:22 -0700

    Recordatorios
    Comentarios agregados a partes del código que pueden resultar útil siempre estar consciente de porqué lo usamos

--- MovimientoRey.cs
+++ MovimientoRey.cs
@@ -17,10 +17,13 @@ namespace PosicionDelReyAjedrez
             //Introducir la posición inicial
             Console.Write("Escriba la posición inicial de su rey:");
             string coordenadas = Console.ReadLine();
-            string[] coordenada = coordenadas.Split(' ');
+            string[] coordenada = coordenadas.Split(' ');//En un array capturamos ambos datos ya que uno es un número y otro una letra
+                                                //  ^^ esta parte hace de separador entre dos variables, puede reemplazarse por
+                                                //cualquier otro caracter para separar las dos variables capturadas, o más
+            
             posiH = Convert.ToChar(coordenada[0]);
             posiv = Convert.ToInt32(coordenada[1]);
-            if (posiH > 'h' || posiv > 9)
+            if (posiH > 'h' || posiv > 8)
             {
                 Console.WriteLine("No es una coordenada existente en un tablero de ajedrez");
                 Console.ReadKey();
@@ -46,7 +49,8 @@ namespace PosicionDelReyAjedrez
                             {
                                 posiv = posiv + 1;
                                 Console.WriteLine("Su movimiento fue:");
-                                Console.Write("R");
+                                Console.Write("R");//Para una notación correcta se escribe la letra que representa la pieza y la coordenada
+                                                   //Como es el rey es; Ra1, por ejemplo
                                 Console.Write(posiH);
                                 Console.WriteLine(posiv);
                                 Console.ReadKey();
